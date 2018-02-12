88b06d80c66efd591a07d6b4bb839961b5b7ad43
ted Liefeld <liefeld@broadinstitute.org>
2018-02-12 09:43:29 -0800

    modify diffzip since the zip file can differ even if the indices are exactly the same so exclude it from the diff

--- tests/GpUnit/diffzip.sh
+++ tests/GpUnit/diffzip.sh
@@ -4,16 +4,19 @@ rm -rf zipTmp
 mkdir zipTmp
 mkdir zipTmp/dir1
 mkdir zipTmp/dir2
-cp $1 zipTmp/dir1
-cp $2 zipTmp/dir2
 
+cp $1  zipTmp/dir1/1.zip
+cp $2  zipTmp/dir2/1.zip
 # unzip the expected file and the job result file
 cd zipTmp/dir1
-unzip $1
+unzip 1.zip
+rm 1.zip 
+
 cd ../../zipTmp/dir2
-unzip $2
+unzip 1.zip
+rm 1.zip
 
 # now diff the dir contents and the first file
+cd ../..
 
-diff zipTmp/dir1 dipTmp/dir2
-diff zipTmp/dir1/genome.ht1 zipTmp/dir2/genome.ht1
+diff zipTmp/dir1 zipTmp/dir2
